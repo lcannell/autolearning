0edd3ca580c5714b6982954c17f15a9568ab88ed
lcannell <loris.cannelli@idsia.ch>
2026-05-11 15:01:06 +0000

    shorten prediction with compact control

--- lane_mpc_qp.py
+++ lane_mpc_qp.py
@@ -44,8 +44,8 @@ from lane_mpc_qp_utils import (
 
 @dataclass
 class MPCParams:
-    prediction_horizon: int = 24
-    control_horizon_fraction: float = 0.25
+    prediction_horizon: int = 20
+    control_horizon_fraction: float = 0.30
     q_delta_u_v: float = 0.10025884372280375
     q_delta_u_psi: float = 0.016572675401761255
 
