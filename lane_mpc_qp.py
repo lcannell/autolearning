54be33314fa29d38630dbc05284217c6cbe9da0f
lcannell <loris.cannelli@idsia.ch>
2026-05-11 15:11:59 +0000

    try eighteen-step seven-control horizon

--- lane_mpc_qp.py
+++ lane_mpc_qp.py
@@ -44,8 +44,8 @@ from lane_mpc_qp_utils import (
 
 @dataclass
 class MPCParams:
-    prediction_horizon: int = 19
-    control_horizon_fraction: float = 0.37
+    prediction_horizon: int = 18
+    control_horizon_fraction: float = 0.39
     q_delta_u_v: float = 0.10025884372280375
     q_delta_u_psi: float = 10.0
 
